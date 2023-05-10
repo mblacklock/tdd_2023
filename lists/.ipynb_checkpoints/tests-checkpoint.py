ac18965b58182c80cdd1a9914595bad728f0b17b
Matthew Blacklock <matthew.blacklock@northumbria.ac.uk>
2023-05-10 15:46:21 +0100

    ..

---
+++ lists/.ipynb_checkpoints/tests-checkpoint.py
@@ -0,0 +1,17 @@
+from django.urls import resolve
+from django.test import TestCase
+
+from lists.views import home_page
+
+# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')  
+        self.assertTemplateUsed(response, 'home.html') 
+        
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
